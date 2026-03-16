098e0513dd8190035ba932cc2d47b0b24891a6f1
libenben2010 <113243023+libenben2010@users.noreply.github.com>
2026-03-16 16:50:45 +0800

    Update libwrt.sh

--- libwrt.sh
+++ libwrt.sh
@@ -1,12 +1,11 @@
-# --- 保持你原有的逻辑 (如果你也想给别的机器编固件就留着) ---
+# --- 雅典娜 LED 逻辑 (按你之前的要求保留) ---
 rm -rf package/emortal/luci-app-athena-led
 git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-app-athena-led
 chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app-athena-led/root/usr/sbin/athena-led
 
-# --- 关键：为红米 AX5 极致瘦身 (新增部分) ---
-# 强制删除大体积的地理位置数据库文件，这能省下约 15MB-20MB 空间
+# --- 极致瘦身：删除 Geodata (这是保证 35MB 目标的关键) ---
+# 删掉这些后，Sing-box 的体积会大幅减小
 rm -rf package/feeds/pw_packages/xray-geodata
 rm -rf package/feeds/packages/v2ray-geodata
 
-# 确保默认 IP 不冲突 (改为红米常用的 31.1，或者你喜欢的)
-sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
+# --- 备注：已移除 IP 修改命令，保持默认 192.168.1.1 ---
